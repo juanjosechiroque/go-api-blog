a058b2a747b5a2e5dc95d925171bbe3c32552b36
Juan Jose Chiroque <juanjosechiroque@gmail.com>
2024-06-16 01:43:06 -0500

    tests: add init unit tests

---
+++ tests/post_test.go
@@ -0,0 +1,64 @@
+package tests
+
+import (
+	"go-api-blog/pkg/routes"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gavv/httpexpect"
+	"github.com/gorilla/mux"
+)
+
+func TestListAllPost(t *testing.T) {
+
+	router := mux.NewRouter()
+
+	allRoutes := routes.GetAllRoutes()
+
+	for _, route := range allRoutes {
+		router.Handle(route.Path, route.Handler).Methods(route.Method)
+	}
+
+	server := httptest.NewServer(router)
+	defer server.Close()
+
+	e := httpexpect.New(t, server.URL)
+
+	e.GET("/posts").Expect().Status(200)
+}
+
+func TestGetPost(t *testing.T) {
+
+	router := mux.NewRouter()
+
+	allRoutes := routes.GetAllRoutes()
+
+	for _, route := range allRoutes {
+		router.Handle(route.Path, route.Handler).Methods(route.Method)
+	}
+
+	server := httptest.NewServer(router)
+	defer server.Close()
+
+	e := httpexpect.New(t, server.URL)
+
+	e.GET("/posts/1").Expect().Status(200)
+}
+
+func TestCreatePost(t *testing.T) {
+
+	router := mux.NewRouter()
+
+	allRoutes := routes.GetAllRoutes()
+
+	for _, route := range allRoutes {
+		router.Handle(route.Path, route.Handler).Methods(route.Method)
+	}
+
+	server := httptest.NewServer(router)
+	defer server.Close()
+
+	e := httpexpect.New(t, server.URL)
+
+	e.POST("/posts").Expect().Status(200)
+}
